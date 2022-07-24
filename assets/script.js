83b3e9938113d9283b30a2cd8e6cbeb32cc9f2bb
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-07-23 17:58:54 -0700

    adds jquery variables

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var myApi = "fd129fded302bf3c8993158fa8a215cc";
+
+var searchBtn = $('#searchButton');
+var clearBtn = $('#clearButton');
+var searchHistory = $('#searchHistory');
+var cityInput = $('#cityInput');
--- script.js
+++
@@ -1,2 +0,0 @@
-var myApi = "fd129fded302bf3c8993158fa8a215cc";
-
